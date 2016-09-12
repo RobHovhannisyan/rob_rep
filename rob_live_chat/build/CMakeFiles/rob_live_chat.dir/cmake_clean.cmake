6a47261f52e2fe22fda0250680ac0040b0a1f238
RobHovhannisyan <rob462547@yandex.ru>
2016-09-12 04:08:06 +0200

    added rob live chat project

---
+++ rob_live_chat/build/CMakeFiles/rob_live_chat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rob_live_chat.dir/main.cpp.o"
+  "rob_live_chat.pdb"
+  "rob_live_chat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rob_live_chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rob_live_chat/user.h
@@ -0,0 +1,8 @@
+#include <string>
+
+class USER
+{
+private:
+    std::string user_name_;
+    std::string user_id_;
+}
